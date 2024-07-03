a60f1268f814f71c25c38f8b8d1eb58f5af38b18
Niklas Molnes Hole <nikasmh@hotmail.com>
2024-07-04 00:03:36 +0200

    Make it possible to give feedback using drawings

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import * as jscad from "@jscad/modeling";
 import { CodeEditor } from "./components/editor/Editor";
 import { Viewer } from "./components/renderer/Viewer";
@@ -16,6 +16,7 @@ import { fixCodeFromImage } from "./prompts/fixCodeFromImage";
 import { fixCodeFromError } from "./prompts/fixCodeFromError";
 import { extractCodeFromMessage } from "./helpers/extractCodeFromMessage";
 import { LLMModel } from "./components/models/llm";
+import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
 
 const initialGeometries = geometryTransformer(booleanExample());
 const model: LLMModel = "claude-3.5";
@@ -27,7 +28,11 @@ export default function Home() {
   const [prompt, setPrompt] = useState("");
   const [basePrompt, setBasePrompt] = useState("Generate a pyramid with a square base.");
   const [loading, setLoading] = useState(false);
+  const [modelImage, setModelImage] = useState<string | null>(null);
   const [image, setImage] = useState<string | null>(null);
+  const canvasRef = useRef<ReactSketchCanvasRef>(null);
+  const resultCanvasRef = useRef<HTMLCanvasElement>(null);
+  const [strokeColor, setStrokeColor] = useState("#000000");
 
   const runPrompt = async (type: "newCode" | "fixWithImage" | "fixWithError") => {
     setLoading(true);
@@ -71,8 +76,8 @@ export default function Home() {
 
       setTimeout(() => {
         const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-        const image = extractImage(canvas);
-        setImage(image);
+        const modelImage = extractImage(canvas);
+        setModelImage(modelImage);
       }, 1000);
     } catch (e: any) {
       const details = extractError(e);
@@ -88,12 +93,39 @@ export default function Home() {
     }
   };
 
+  const mergeCanvas = async () => {
+    const drawing = await canvasRef.current?.exportImage("png"); // Base64 string
+    const resultCanvas = resultCanvasRef.current;
+
+    if (drawing && modelImage && resultCanvas) {
+      const context = resultCanvas.getContext("2d");
+
+      if (context) {
+        context.clearRect(0, 0, resultCanvas.width, resultCanvas.height);
+
+        const modelImageEl = new Image();
+        modelImageEl.src = modelImage;
+        modelImageEl.onload = () => {
+          context.drawImage(modelImageEl, 0, 0);
+
+          const drawingEl = new Image();
+          drawingEl.src = drawing;
+          drawingEl.onload = () => {
+            context.drawImage(drawingEl, 0, 0);
+
+            setImage(resultCanvas.toDataURL());
+          };
+        };
+      }
+    }
+  };
+
   return (
     <main className="w-screen h-screen flex flex-col">
       <h1 className="text-4xl text-center my-4">ClaudeCAD</h1>
       <div className="relative flex-1 flex flex-col">
         <CodeEditor {...{ code, setCode }} />
-        {image && (
+        {modelImage && (
           <button onClick={() => runPrompt("fixWithImage")} className="btn btn-success self-center">
             Evaluate
             {loading && <span className="loading loading-spinner loading-sm"></span>}
@@ -106,7 +138,7 @@ export default function Home() {
             value={basePrompt}
             onChange={(e) => setBasePrompt(e.target.value)}
           />
-          {(image || error) && (
+          {(modelImage || error) && (
             <textarea
               className="flex-1 input input-primary py-1 px-2 h-[60px]"
               value={prompt}
@@ -137,10 +169,45 @@ export default function Home() {
           </div>
         </div>
 
-        <Viewer geometries={geometries} />
+        <div className="relative" style={{ width: 256, height: 256 }}>
+          <Viewer geometries={geometries} />
+          <ReactSketchCanvas
+            ref={canvasRef}
+            width="256px"
+            height="256px"
+            strokeColor={strokeColor}
+            canvasColor="transparent"
+            className="absolute top-0 left-0"
+          />
+          <canvas
+            ref={resultCanvasRef}
+            onClick={mergeCanvas}
+            width="256"
+            height="256"
+            className="absolute top-0 left-[110%] border-2 border-red-300 rounded-md"
+          />
+        </div>
+        <div>
+          <button onClick={() => setStrokeColor("#ff0000")} className="btn bg-red-300 hover:bg-red-300/80 text-black">
+            Red
+          </button>
+          <button
+            onClick={() => setStrokeColor("#00ff00")}
+            className="btn bg-green-300 hover:bg-green-300/80 text-black"
+          >
+            Green
+          </button>
+          <button onClick={() => setStrokeColor("#0000ff")} className="btn bg-blue-300 hover:bg-blue-300/80 text-black">
+            Blue
+          </button>
+          <button onClick={() => canvasRef.current?.clearCanvas()} className="btn btn-error">
+            Clear
+          </button>
+        </div>
         {error && (
           <div className="fixed bottom-0 left-0 right-0 text-red-500 bg-black/20 whitespace-pre-wrap p-4">{error}</div>
         )}
+        {/*<img src="/test.svg" alt="test" />*/}
       </div>
     </main>
   );
