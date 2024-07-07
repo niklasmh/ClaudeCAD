d8bd76bc22543f64b249baf2d19f15744d6c5226
Niklas Molnes Hole <nikasmh@hotmail.com>
2024-07-07 05:31:51 +0200

    Make code messages hidden is set

--- app/components/chat/CodeMessage.tsx
+++ app/components/chat/CodeMessage.tsx
@@ -1,5 +1,5 @@
 import { LLMCodeMessage, modelNames } from "@/app/types/llm";
-import { Pencil, Play, Save, Trash } from "lucide-react";
+import { Eye, Pencil, Play, Save, Trash } from "lucide-react";
 import { useState } from "react";
 import { CodeEditor } from "../editor/Editor";
 
@@ -29,6 +29,10 @@ export const CodeMessage = ({ message, onChange, onRun, onDelete }: Props) => {
     setCurrentMessage({ ...currentMessage, text: code });
   };
 
+  const handleShow = () => {
+    onChange({ ...currentMessage, hidden: false });
+  };
+
   const tools = (
     <div
       className={`absolute group-hover:visible invisible top-0 bottom-0 flex flex-row items-center mx-3 gap-2 opacity-30 ${
@@ -54,12 +58,32 @@ export const CodeMessage = ({ message, onChange, onRun, onDelete }: Props) => {
     </div>
   );
 
+  const hidden = (
+    <div
+      className={`absolute group-hover:visible invisible top-0 bottom-0 flex flex-row items-center mx-3 gap-2 opacity-30 ${
+        isUser ? "right-full flex-row-reverse" : "left-full"
+      }`}
+    >
+      <div onClick={handleShow} className="hover:opacity-80 cursor-pointer" title="Show message">
+        <Eye size={16} />
+      </div>
+      <div onClick={onDelete} className="hover:text-red-300 cursor-pointer" title="Remove message">
+        <Trash size={16} />
+      </div>
+    </div>
+  );
+
   return (
     <div className={`chat group ${isUser ? "chat-end pl-12" : "chat-start pr-12"}`}>
       <div className="chat-header">{modelNames[message.model]}</div>
       <div className={`chat-bubble relative w-full p-3 ${isUser ? "bg-[#2a323c88]" : ""}`}>
-        <CodeEditor readOnly={!edit} code={currentMessage.text} setCode={handleCodeChange} />
-        {tools}
+        {!message.hidden && <CodeEditor readOnly={!edit} code={currentMessage.text} setCode={handleCodeChange} />}
+        {message.hidden && (
+          <div className="text-gray-400">
+            <i>{message.hiddenText || "This code is hidden"}</i>
+          </div>
+        )}
+        {message.hidden ? hidden : tools}
       </div>
       <div className="chat-footer opacity-50">Sent {new Date(message.date).toLocaleString()}</div>
     </div>
