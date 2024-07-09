a3fe1d75e4a0e399bcac567ced05d053a3f73727
Niklas Molnes Hole <nikasmh@hotmail.com>
2024-07-10 01:40:01 +0200

    Make header not fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,15 @@ import { Chat } from "./components/chat/Chat";
 
 export default function Home() {
   return (
-    <main className="w-screen max-w-[800px] mx-auto pt-[180px] pb-[100px] px-4">
+    <main className="w-screen max-w-[800px] mx-auto pb-[100px] px-4">
       <div
-        className="fixed top-0 left-0 right-0 bg-base-100 z-20 pt-4 pb-6 px-4"
+        className="w-full bg-base-100 z-20 pt-4 pb-12 px-4"
         style={{
           boxShadow: "0 0 20px 5px var(--fallback-b1,oklch(var(--b1)))",
         }}
       >
         <div className="flex flex-row items-center gap-6 max-w-[420px] mx-auto">
-          <img
-            src="/icon.png"
-            alt="Logo"
-            className="w-[100px] h-[100px] mx-auto mt-4 mix-blend-color-dodge rounded-2xl"
-          />
+          <img src="/icon.png" alt="Logo" className="w-[100px] h-[100px] mx-auto mt-4" />
           <div className="flex-1">
             <h1 className="text-4xl my-4">ClaudeCAD</h1>
             <p className="text-base">Making models for 3D printing intuitive.</p>
