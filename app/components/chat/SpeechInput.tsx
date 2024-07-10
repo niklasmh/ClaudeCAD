c1089aa69c4e32d4c196fac28955047cd33e3cc0
Niklas Molnes Hole <nikasmh@hotmail.com>
2024-07-10 06:33:14 +0200

    Set a timer for STT and clean up

--- app/components/chat/SpeechInput.tsx
+++ app/components/chat/SpeechInput.tsx
@@ -1,37 +1,38 @@
 import { useAppStore } from "@/app/store";
 import { Mic } from "lucide-react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+
+let timer: any;
 
 export const SpeechInput = () => {
   const setTextInput = useAppStore((state) => state.setTextInput);
-  const { transcript, listening } = useSpeechRecognition();
-
-  const registeredListening = useRef(true);
+  const { transcript, listening, resetTranscript } = useSpeechRecognition();
 
   useEffect(() => {
-    if (listening || !transcript || registeredListening.current) return;
+    setTextInput(transcript);
+  }, [transcript, setTextInput]);
 
-    registeredListening.current = true;
+  const toggleListening = () => {
+    if (listening) {
+      SpeechRecognition.stopListening();
+    } else {
+      resetTranscript();
+      SpeechRecognition.startListening({ continuous: true, language: "en-US" });
+    }
+  };
 
-    const run = async () => {
-      setTextInput(transcript);
-    };
-    run();
-  }, [listening, transcript, setTextInput]);
+  useEffect(() => {
+    if (transcript) {
+      clearTimeout(timer!);
+      timer = setTimeout(() => {
+        SpeechRecognition.stopListening();
+      }, 5000);
+    }
+  }, [transcript]);
 
   return (
-    <button
-      className={"btn btn-outline "}
-      onClick={() => {
-        if (listening) {
-          SpeechRecognition.stopListening();
-        } else {
-          registeredListening.current = false;
-          SpeechRecognition.startListening();
-        }
-      }}
-    >
+    <button className={"btn btn-outline "} onClick={toggleListening}>
       {listening ? <div className="loading loading-ball loading-xs" /> : <Mic size={16} />}
     </button>
   );
