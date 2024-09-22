40413f7206cd2a8e7756974bd15497699efe732f
Niklas Molnes Hole <nikasmh@hotmail.com>
2024-09-22 21:18:43 +0200

    Add image input

--- app/components/chat/SketchInput.tsx
+++ app/components/chat/SketchInput.tsx
@@ -1,4 +1,4 @@
-import { Check, Eraser, Trash } from "lucide-react";
+import { Check, Eraser, Image, Trash } from "lucide-react";
 import { ForwardedRef, forwardRef, useEffect, useState } from "react";
 import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
 
@@ -43,9 +43,22 @@ export const SketchInput = forwardRef(
   ) => {
     const [strokeColor, setStrokeColor] = useState("#000000");
     const [eraser, setEraser] = useState(false);
+    const [backgroundImage, setBackgroundImage] = useState<string | undefined>();
 
     const handleToggleEraser = () => {
       setEraser((e) => !e);
+    };
+
+    const handleImageInput = (e: any) => {
+      if (!e.target.files.length) return;
+
+      const reader = new FileReader();
+
+      reader.addEventListener("load", (evt: any) => {
+        setBackgroundImage(evt.target.result);
+      });
+
+      reader.readAsDataURL(e.target.files[0]);
     };
 
     useEffect(() => {
@@ -63,10 +76,11 @@ export const SketchInput = forwardRef(
           canvasColor={transparent ? "transparent" : "#ffffff"}
           className="!rounded-lg overflow-hidden !border-none"
           {...canvasProps}
+          {...(backgroundImage ? { backgroundImage, preserveBackgroundImageAspectRatio: "xMidYMid" } : {})}
         />
 
         {showControls && (
-          <div className="flex flex-row gap-2">
+          <div className="flex flex-row gap-2 items-center">
             {colors.map(({ name, value, className }) => (
               <button
                 key={value}
@@ -74,16 +88,20 @@ export const SketchInput = forwardRef(
                   setStrokeColor(value);
                   setEraser(false);
                 }}
-                className={`btn btn-sm aspect-square ${className} p-0`}
+                className={`btn btn-sm rounded-full h-6 min-h-6 w-6 aspect-square ${className} p-0`}
                 title={name}
               >
-                {!eraser && strokeColor === value ? <Check /> : ""}
+                {!eraser && strokeColor === value ? <Check size={16} /> : ""}
               </button>
             ))}
             <div className="divider divider-horizontal m-0" />
+            <label className={`btn btn-primary btn-sm aspect-square p-0 ${eraser ? "" : "btn-ghost"}`}>
+              <Image size={20} />
+              <input type="file" accept="image/png, image/jpeg" className="hidden" onChange={handleImageInput} />
+            </label>
             <button
               onClick={handleToggleEraser}
-              className={`btn btn-primary btn-sm aspect-square p-0 ${eraser ? "" : "btn-ghost"}`}
+              className={`btn btn-primary btn-sm aspect-square p-0 -mx-2 ${eraser ? "" : "btn-ghost"}`}
             >
               <Eraser size={20} />
             </button>
